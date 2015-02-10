1e91aae11fe3534c8a8e1b2e2150ba1737eab7db
Miguel <mg.arreaza.14@outlook.com>
2015-02-10 17:18:36 -0430

    Proyecto Udo

---
+++ src/hipico/Hipico.java
@@ -0,0 +1,15 @@
+
+package hipico;
+
+public class Hipico {
+
+   
+    public static void main(String[] args) {
+        
+        Formulario obj = new Formulario();
+        obj.setVisible(true);
+    }
+
+    
+    
+}
